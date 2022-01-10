642912c5fc0a2420531361b2548c7056ee21a9e1
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmail.com>
2022-01-10 22:42:25 +0700

    Add project files.

---
+++ WCFService_2Way_115/ServiceCallback.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_115
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode =InstanceContextMode.Single)]
+    public class ServiceCallback : IServiceCallback
+    {
+        Dictionary<IClientCallback, string> userList = new Dictionary<IClientCallback, string>(); //Menyimpan data ketika user online
+        public void gabung(string username)
+        {
+            IClientCallback koneksiGabung = OperationContext.Current.GetCallbackChannel<IClientCallback>(); //untuk menampung user ketika baru daftar/buat akun
+            userList[koneksiGabung] = username;
+        }
+
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void kirimPesan(string pesan)
+        {
+            IClientCallback koneksiPesan = OperationContext.Current.GetCallbackChannel<IClientCallback>(); //mengirim data user dan pesan ke user lain
+            string user;
+            if(!userList.TryGetValue(koneksiPesan, out user))
+            {
+                return;
+            }
+            foreach (IClientCallback other in userList.Keys)
+            {
+                other.pesanKirim(user, pesan);
+            }
+        }
+    }
+}
